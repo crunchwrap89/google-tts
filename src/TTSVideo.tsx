886b407fda6ad5d3324424352f7ddfc602ffb2ac
elmaco <marcus89n@gmail.com>
2025-12-28 21:10:07 +0100

    Refactor code

--- src/TTSVideo.tsx
+++ src/TTSVideo.tsx
@@ -1,5 +1,6 @@
 import {
   AbsoluteFill,
+  Html5Audio,
   interpolate,
   Sequence,
   useCurrentFrame,
@@ -10,6 +11,9 @@ import { voices } from "./server/TextToSpeech/constants";
 import { RequestMetadata, VoiceType } from "./lib/interfaces";
 import { z } from "zod";
 import { zColor } from "@remotion/zod-types";
+import { SERVER_URL } from "./server/TextToSpeech/constants";
+import { AudioVisualizer } from "./TTSVideo/AudioVisualizer";
+import { SyncStatus } from "./TTSVideo/SyncStatus";
 
 export const mySchema = z.object({
   captionText: z.string(),
@@ -49,10 +53,26 @@ export const TTSVideo: React.FC<RequestMetadata> = (props) => {
   );
   const transitionStart = 25;
 
+  const proxiedUrl = props.audioUrl
+    ? `${SERVER_URL}/proxy?url=${encodeURIComponent(props.audioUrl)}`
+    : null;
+
   return (
     <AbsoluteFill style={{ flex: 1, backgroundColor: "white" }}>
       <div style={{ opacity }}>
         <Sequence from={transitionStart + 10}>
+          {props.debug && (
+            <SyncStatus
+              isSynced={!!(props.timepoints && props.timepoints.length > 0)}
+            />
+          )}
+
+          {proxiedUrl && (
+            <Html5Audio id="TTS Audio" about="TTS Audio" src={proxiedUrl} />
+          )}
+
+          {proxiedUrl && <AudioVisualizer audioUrl={props.audioUrl!} />}
+
           <Captions {...props} />
         </Sequence>
       </div>
--- src/TTSVideo/Captions.tsx
+++ src/TTSVideo/Captions.tsx
@@ -1,71 +1,23 @@
-import {
-  Html5Audio,
-  useVideoConfig,
-} from "remotion";
-import { SERVER_URL } from "../server/TextToSpeech/constants";
 import { RequestMetadata } from "../lib/interfaces";
-import { AudioVisualizer } from "./AudioVisualizer";
-import { splitIntoSentences } from "../lib/text-utils";
-import React, { useCallback, useMemo } from "react";
-import { SyncStatus } from "./SyncStatus";
+import React from "react";
 import { SentenceRenderer } from "./SentenceRenderer";
+import { useSentenceTiming } from "./hooks/useSentenceTiming";
 
-// Force TS check
 export const Captions: React.FC<RequestMetadata> = (props) => {
   const {
     captionText,
     captionColor,
     timepoints,
-    debug = false,
     animationStyle = "pop",
   } = props;
-  const videoConfig = useVideoConfig();
 
-  const audioDurationFrames = Math.max(1, videoConfig.durationInFrames - 70);
-  const captionTextForAnimation = captionText
-    .trim()
-    .split(/\s+/)
-    .map((t) => ` ${t} `);
-  const delayPerWord = audioDurationFrames / captionTextForAnimation.length;
-
-  const proxiedUrl = props.audioUrl
-    ? `${SERVER_URL}/proxy?url=${encodeURIComponent(props.audioUrl)}`
-    : null;
-
-  if (!timepoints || timepoints.length === 0) {
-    console.warn("No timepoints available for text animation sync.");
-  }
-
-  const getStartTime = useCallback((index: number) => {
-    const timepoint = timepoints?.find((tp) => tp.markName === `word_${index}`);
-    return timepoint
-      ? Number(timepoint.timeSeconds) * videoConfig.fps
-      : index * delayPerWord;
-  }, [delayPerWord, timepoints, videoConfig.fps]);
-
-  const sentencesWithTiming = useMemo(() => {
-    const sentences = splitIntoSentences(captionText);
-    return sentences.map((s) => {
-      const firstWordIndex = s.wordIndices[0];
-      const startTime = getStartTime(firstWordIndex);
-      return { ...s, startTime };
-    });
-  }, [captionText, getStartTime]);
+  const { sentencesWithTiming, getStartTime, durationInFrames } = useSentenceTiming(captionText, timepoints);
 
   return (
     <>
-      {debug && <SyncStatus isSynced={!!(timepoints && timepoints.length > 0)} />}
-
-      {proxiedUrl && (
-        <Html5Audio id="TTS Audio" about="TTS Audio" src={proxiedUrl} />
-      )}
-
-      {proxiedUrl && <AudioVisualizer audioUrl={props.audioUrl!} />}
-
       {sentencesWithTiming.map((sentence, index) => {
         const nextSentenceStartTime =
-          sentencesWithTiming[index + 1]?.startTime ??
-          videoConfig.durationInFrames;
+          sentencesWithTiming[index + 1]?.startTime ?? durationInFrames;
 
         return (
           <SentenceRenderer
